a433c80748fd08114e8e3988a60d9d471fc38670
NajamShehzad <najambutt195@gmail.com>
2018-09-07 22:52:29 +0500

    Getting List using Axios

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header/header'
+import Header from './src/components/header/header';
+import AlbumList from './src/components/AlbumList/AlbumList';
 export default class App extends React.Component {
   render() {
     return (
       <View>
         <Header name={"Header Albums"} />
+        <AlbumList/>
       </View>
     );
   }
--- src/components/AlbumList/AlbumList.js
+++ src/components/AlbumList/AlbumList.js
@@ -1,16 +1,30 @@
-import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
 
 
-class AlbumList extends Component{
-    render(){
-        return(
+class AlbumList extends Component {
+    state ={
+        albums:[]
+    }
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(res => this.setState({albums:res.data}))
+        .catch(err => console.log(err));
+        // console.log("Is it working?");
+    }
+    render() {
+        const {albums} = this.state;
+        return (
             <View>
                 <Text>
-                    This is Album
+                    This is Album!
                 </Text>
+                {albums.map(data => <Text key={data.title}>{data.title}</Text>)}
             </View>
         )
     }
 
 }
+export default AlbumList;
